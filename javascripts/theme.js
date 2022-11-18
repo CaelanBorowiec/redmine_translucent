d47baf6db4dfd4456c23ad97a5b96e8f233c4538
Caelan Borowiec <caelan@caelanb.me>
2022-11-18 16:47:13 -0500

    Squashed commit of the following:
    commit 366405e347eba1c0d9a1564b89bb1c4ada46608d
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 16:44:16 2022 -0500
    
        add photos + metadata
    
    commit 6d63d6a51a97e810ebc295ccc4377cd8b5fbceab
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 16:37:27 2022 -0500
    
        smart footer credits
    
    commit ca863639685b9bdd330e2c057bd9f828960f1f9b
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 16:11:30 2022 -0500
    
        invoke wallpaper randomizer after page load
    
        avoid trying to change wallpaper before CSS loads
    
    commit f9500120509b067354da989b530afac8e2103c05
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 16:09:06 2022 -0500
    
        background picker logic
    
    commit be183ab502d87871c4bf866245506759c1ca469c
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 15:32:12 2022 -0500
    
        scrap extra json file since something in redmine is breaking requests
    
    commit f0543758b1d44b1b85ea26f6d93a7b008188a2f0
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 15:27:30 2022 -0500
    
        json request fixes
    
    commit 589020a496e99b551743778c89b5fae4e1907e92
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 15:01:40 2022 -0500
    
        forgot javascripts folder
    
    commit 597c675b2213ada11700172aa117f7f6cc47a9e9
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 15:00:51 2022 -0500
    
        use full path
    
    commit c4235ae18f8c7f4e67143f00739a17f93462f299
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 14:52:21 2022 -0500
    
        update wallpapers.json path
    
    commit ac397c75bbc594f730c13e7f9e060fd8b737ab6e
    Author: Caelan Borowiec <caelan@caelanb.me>
    Date:   Fri Nov 18 14:48:35 2022 -0500
    
        wallpaper library test

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,5 +1,6 @@
  if (window.jQuery) {
    $(document).ready(function() {
+     wallpaperPicker();
      if (window.devicePixelRatio > 1) {
        var images = findImagesByRegexp('contacts_thumbnail', document);
 
@@ -22,6 +23,7 @@
    });
  } else {
    document.observe("dom:loaded", function() {
+     wallpaperPicker();
      if (window.devicePixelRatio > 1) {
        var images = findImagesByRegexp('thumbnail', document);
 
@@ -43,6 +45,64 @@
      }
 
    });
+ }
+
+ function wallpaperPicker() {
+   (function() {
+     const wallpapers = {
+       "backgrounds": [{
+           "name": "",
+           "file": "antelope_canyon.avif",
+           "creator": "Vlad D",
+           "credit_url": "https://unsplash.com/photos/hEFQnP_i1eY",
+           "theme_subclass": ""
+         },
+         {
+           "name": "",
+           "file": "cumulus_clouds.avif",
+           "creator": "Zbynek Burival",
+           "credit_url": "https://unsplash.com/photos/8iZG31eXkks",
+           "theme_subclass": ""
+         },
+         {
+           "name": "",
+           "file": "fall_lake.avif",
+           "creator": "Aaron Burden",
+           "credit_url": "https://unsplash.com/photos/aj_NElqhu6Q",
+           "theme_subclass": ""
+         },
+         {
+           "name": "",
+           "file": "yellow_wave.avif",
+           "creator": "Texco Kwok",
+           "credit_url": "https://unsplash.com/photos/Otz-5OfAiA4",
+           "theme_subclass": ""
+         },
+         {
+           "name": "",
+           "file": "greece_clouds.avif",
+           "creator": "Jason Blackeye",
+           "credit_url": "https://unsplash.com/photos/ap3LXI0fPJY",
+           "theme_subclass": ""
+         }
+       ]
+     };
+
+     // Get a "random" number that changes hourly wrapped to the number of backgrounds
+     let i = function(max = wallpapers.backgrounds.length) {
+       let date = new Date();
+       const mod = function(x, m) {
+         return (x % m + m) % m;
+       }
+       let pos = Math.abs(date.getDate() - date.getHours());
+       return mod(pos, max);
+     }()
+
+     let current = wallpapers.backgrounds[i];
+     $("#wrapper").css("background-image", `url(/themes/redmine_translucent/images/wallpapers/${current.file})`);
+     $('<span>').addClass('footernote').html(`<a href="https://github.com/CaelanBorowiec/redmine_translucent" target="_blank">Redmine Translucent</a> Theme by <a href="https://caelanb.me/" target="_blank">Caelan Borowiec</a>`).appendTo('#footer');
+     $('<span>').addClass('footernote').html(`Photo ${current.name} by <a href="${current.credit_url}" target="_blank">${current.creator}</a>`).appendTo('#footer');
+   })();
  }
 
  function findImagesByRegexp(regexp, parentNode) {
